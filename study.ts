0f8dff584819bb309cafdeb82b1fc82ebdabdb8a
Ivan Kalashnikov <111391684+ivankalashnikov89@users.noreply.github.com>
2023-09-22 21:33:09 -0700

    Update study.ts

--- study.ts
+++ study.ts
@@ -2,6 +2,49 @@
 
 //https://www.typescriptlang.org
 
+/*TypeScript (TS) is a statically typed superset of 
+JavaScript that adds optional static typing, advanced 
+tooling, and improved developer experiences to 
+JavaScript-based web and application development. 
+It compiles to plain JavaScript, making it compatible 
+with all JavaScript environments. TypeScript helps 
+catch type-related errors at compile-time, provides 
+enhanced code navigation and autocompletion, and is 
+widely used for building scalable and maintainable web 
+and software applications. */
+
+//TypeScript Data Types
+
+//Assigning Types
+
+//Structural Types
+
+//Object Types
+
+//Array Types
+
+//Tuple Types
+
+//Enum Types
+
+//Any Types
+
+//Uniion Types
+
+//Literal Ty[es
+
+//Function Types
+
+
+
+//Interfaces
+
+//Modules
+
+//Classes
+
+//
+
 const age:number = 33;
 const myName:string = "Ivan";
 const isMarried:boolean = false;
